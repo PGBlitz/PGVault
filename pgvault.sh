e3550357f8e0439c416c44d99ce29afeb9ab268c
Admin9705 <user@dunn.cloud>
2019-01-24 21:05:08 -0500

    update

--- pgvault.sh
+++ pgvault.sh
@@ -5,9 +5,8 @@
 # URL:        https://plexguide.com - http://github.plexguide.com
 # GNU:        General Public License v3.0
 ################################################################################
-source /opt/plexguide/menu/functions/functions.sh
-source /opt/plexguide/menu/functions/pgvault.func
-
+source /opt/pgvault/functions.sh
+source /opt/pgvault/pgvault.func
 file="/var/plexguide/restore.id"
 if [ ! -e "$file" ]; then
   echo "[NOT-SET]" > /var/plexguide/restore.id
