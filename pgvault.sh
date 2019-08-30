796422e487e1d46dd1941779fd57361e3eeb2d32
Admin9705 <24727006+Admin9705@users.noreply.github.com>
2019-08-30 08:03:53 -0400

    update

--- pgvault.sh
+++ pgvault.sh
@@ -6,7 +6,7 @@
 # GNU:        General Public License v3.0
 ################################################################################
 source /pg/pgvault/functions/functions.sh
-source /pg/pgvault/pgvault.func
+source /pg/pgvault/functions/pgvault.func
 file="/pg/var/restore.id"
 if [ ! -e "$file" ]; then
   echo "[NOT-SET]" >/pg/var/restore.id
